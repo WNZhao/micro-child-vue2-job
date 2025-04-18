925d8330749415e85a09b0d1c5dc891d0470b0b4
Walker <zw37520@gmail.com>
2025-04-18 18:24:16 +0700

    fix: 优化搜索框样式和交互

--- src/views/FindJob.vue
+++ src/views/FindJob.vue
@@ -2,7 +2,7 @@
  * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
  * @Date: 2025-04-11 14:09:06
  * @LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
- * @LastEditTime: 2025-04-13 16:28:26
+ * @LastEditTime: 2025-04-18 13:57:05
  * @FilePath: /micro-child-vue2-job/src/views/FindJob.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
